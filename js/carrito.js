133928f3c0ebd65173828e91abc5a67482555f25
Angelo Zamora <angelozamora987@gmail.com>
2020-05-30 02:15:06 -0500

    Se agrego lista carrito

---
+++ js/carrito.js
@@ -0,0 +1,69 @@
+//VARIABLES
+var carritocontent=document.querySelector(".carrito-content");
+var listacompras=document.querySelector("#lista-compras tbody");
+var vaciarcarrito=document.querySelector("#vaciar-carrito");
+
+
+//EVENTLISTENER
+eventListener();
+
+function eventListener(){
+    carritocontent.addEventListener("click",agregarCompra);
+    vaciarcarrito.addEventListener("click",vaciarCarro);
+    listacompras.addEventListener("click",borarCompra);
+}
+
+
+//FUNCTION
+function agregarCompra(e){
+    e.preventDefault();
+    if(e.target.classList.contains("boton-compra")){
+        var infoCompra=e.target.parentElement;
+        console.log(infoCompra.querySelector("img"));
+    }
+
+    leerCompra(infoCompra)
+}
+
+function leerCompra(compra){
+    var infoRegalo={
+        imagen : compra.querySelector("img").src,
+        direccion : compra.querySelector("img").alt,
+        id : compra.querySelector("img").getAttribute("data-id")
+    }
+
+    agregarCarrito(infoRegalo);
+}
+
+function agregarCarrito(regalo){
+    let row=document.createElement("tr");
+
+    row.innerHTML=`
+        <td class="td1">
+            <img src="${regalo.imagen}" alt="" class="regaloImg">
+        </td>
+        <td><a class="ver-url" href="${regalo.direccion}">VER</a></td>
+        <td>
+            <a href="#" class="borrar-curso"  data-id=${regalo.id}>X</a>
+        </td>
+    `;
+
+    listacompras.appendChild(row);
+
+
+}
+
+function vaciarCarro(){
+    listacompras.innerHTML="";
+}
+
+function borarCompra(e){
+    e.preventDefault();
+    if(e.target.classList.contains("borrar-curso")){
+        var cursoBorar=e.target.parentElement.parentElement;
+        cursoBorar.remove();
+    }
+    else{
+        console.log("no");
+    }
+}
